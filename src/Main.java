71b6efcb1eaf6ec13eb0ea1cb6d9049b8d19cb51
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-12-01 00:47:11 -1000

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,31 @@ public class Main {
     private static Node starter;
     private static List<Node> nodes;
 
+    /**
+     * The main method that takes in user input.
+     * Prints the neighboring nodes of each node.
+     * @param args
+     */
     public static void main(String[]args) {
         nodes = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         String nodeNames = s.nextLine();
         String[] names = nodeNames.split(" ");
+        //Instantiate a Node object for each encountered Node name.
         for(var name: names) {
             Node n = new Node(name);
             nodes.add(n);
         }
+        //The starting Node of the traversal.
         String starterName = s.nextLine();
         starter = findNodeByName(starterName);
         while(true) {
             String a = s.next();
+            //Breaks the loop when the last line of "***" is encountered.
             if(a.equals("***")) {
                 break;
             }
+            //Add the nodes into the current Node's neighbors ArrayList.
             Node n1 = findNodeByName(a);
             String b = s.next();
             Node n2 = findNodeByName(b);
@@ -43,11 +52,16 @@ public class Main {
             n.visited = false;
         }
 
-        System.out.println();
+        System.out.println("");
         System.out.println("Breath-First Search: ");
         breathFirst(starter);
     }
 
+    /**
+     * Returns the Node object by passing the name into the parameter.
+     * @param name Node name
+     * @return Node object
+     */
     private static Node findNodeByName(String name) {
         for(Node n: nodes) {
             if(name.equals(n.name)) {
@@ -57,9 +71,15 @@ public class Main {
         return null;
     }
 
+    /**
+     * Prints the nodes in depth-first traversal oder.
+     * @param n Node object
+     */
     private static void depthFirst(Node n) {
+        //Marks eac visited node and prints them.
         n.visited = true;
         System.out.print(n.name + " ");
+        //Loops through the current node's neighboring nodes and calls the recursive function.
         for(Node neighborNode : n.neighbors) {
             if(!neighborNode.visited) {
                 depthFirst(neighborNode);
@@ -67,10 +87,17 @@ public class Main {
         }
     }
 
+    /**
+     * Prints the nodes in breadth-first traversal order.
+     * @param n Node object
+     */
     private static void breathFirst(Node n) {
+        //Acts as a placeholder to keep track of the breadth-first traversal.
         List<Node> breadthFirstNodes = new ArrayList<>();
+        //Marks each visited node and prints them.
         n.visited = true;
         System.out.print(n.name + " ");
+        //The first Node in the placeholder list will be looped to mark its neighboring nodes as visited.
         breadthFirstNodes.add(n);
         while(breadthFirstNodes.size() != 0) {
             for(Node neighborNode : breadthFirstNodes.get(0).neighbors) {
@@ -80,9 +107,9 @@ public class Main {
                     breadthFirstNodes.add(neighborNode);
                 }
             }
+            //If all neighboring nodes of the current node are visited, removes from the list.
             breadthFirstNodes.remove(0);
         }
+        //The loop will start at the next Node which is now the first Node of the placeholder list.
     }
-
-
 }
